4b5350bbcb12b54acff3f288e014abadd9b5b6c6
papiando (HOME) <info@papiando.com>
2019-02-22 00:11:27 -0400

    Update article.view.php

--- view/article.view.php
+++ view/article.view.php
@@ -7,8 +7,19 @@ class ArticleView extends View {
 	protected $listColumns = "title,status,category,language,accesslevel";
 	
 	// Form definition
-	public function getDefinition($new = false) {
+	protected function getDefinition(&$_Data = null) {
+		$new = is_null($_Data);
 		return (object)[
+			'top'=>[
+				'columns'=>[ 'left'=>'col-8','right'=>'col-4' ],
+				'left'=>[
+					[ 'name'=>'id', 'prefix'=>($new ? '' : ':'), 'type'=>'hidden', 'value'=>($_Data->{'#'} ?? '') ],
+					[ 'name'=>'title', 'class'=>'form-control', 'prefix'=>($new ? ':' : ''), 'required'=>true, 'title'=>'Title', 'type'=>'text', 'value'=>($_Data->title ?? '') ]
+				],
+				'right'=>[
+					[ 'name'=>'name', 'class'=>'form-control', 'prefix'=>($new ? ':' : ''), 'required'=>true, 'title'=>'Alias', 'type'=>'text', 'value'=>($_Data->name ?? '') ]
+				]
+			],
 			'tabs'=>[
 				[ 'tab'=>'content-tab', 'pane'=>'content-pane', 'selected'=>true, 'title'=>'Content' ],
 				[ 'tab'=>'image-tab', 'pane'=>'image-pane', 'selected'=>false, 'title'=>'Image and Metadata' ],
@@ -18,53 +29,53 @@ class ArticleView extends View {
 			'content-pane'=>[
 				'columns'=>[ 'left'=>'col-12 col-md-8','right'=>'col-12 col-md-4' ],
 				'left'=>[
-					[ 'name'=>'intro', 'class'=>'form-control text-html', 'required'=>true, 'size'=>4, 'title'=>ucwords($this->class).' Intro', 'type'=>'textarea' ],
-					[ 'name'=>'body', 'class'=>'form-control text-html', 'required'=>true, 'size'=>8, 'title'=>ucwords($this->class).' Body', 'type'=>'textarea' ]
+					[ 'name'=>'intro', 'class'=>'form-control text-html', 'required'=>true, 'size'=>4, 'title'=>ucwords($this->class).' Intro', 'type'=>'textarea', 'value'=>($_Data->intro ?? '') ],
+					[ 'name'=>'body', 'class'=>'form-control text-html', 'required'=>true, 'size'=>8, 'title'=>ucwords($this->class).' Body', 'type'=>'textarea', 'value'=>($_Data->body ?? '') ]
 				],
 				'right'=>[
-					[ 'name'=>'status', 'class'=>'form-control form-control-sm', 'default'=>Application::getController()->canPublish() ? STATUS_PUBLISHED : STATUS_UNPUBLISHED, 'prefix'=>($new ? ':' : ''), 'query'=>Form::query('status',Session::isAccessible()), 'readonly'=>Application::getController()->cannotPublish(), 'title'=>'Status', 'type'=>'select' ],
-					[ 'name'=>'category', 'class'=>'form-control form-control-sm', 'default'=>CATEGORY_UNDEFINED, 'prefix'=>($new ? ':' : ''), 'query'=>Form::query('category',Session::isAccessible()), 'title'=>'Category', 'type'=>'select' ],
-					[ 'name'=>'language', 'class'=>'form-control form-control-sm', 'default'=>LANGUAGE_UNDEFINED, 'prefix'=>($new ? ':' : ''), 'query'=>Form::query('language',Session::isAccessible()), 'title'=>'Language', 'type'=>'select' ],
-					[ 'name'=>'accesslevel', 'class'=>'form-control form-control-sm', 'default'=>ACCESS_PUBLIC, 'prefix'=>($new ? ':' : ''), 'query'=>Form::query('accesslevel',Session::isAccessible()), 'title'=>'Access Level', 'type'=>'select' ]
+					[ 'name'=>'status', 'class'=>'form-control form-control-sm', 'prefix'=>($new ? ':' : ''), 'query'=>Form::query('status',Session::isAccessible()), 'readonly'=>Application::getController()->cannotPublish(), 'title'=>'Status', 'type'=>'select', 'value'=>($_Data->status ?? (Application::getController()->canPublish() ? STATUS_PUBLISHED : STATUS_UNPUBLISHED)) ],
+					[ 'name'=>'category', 'class'=>'form-control form-control-sm', 'prefix'=>($new ? ':' : ''), 'query'=>Form::query('category',Session::isAccessible()), 'title'=>'Category', 'type'=>'select', 'value'=>($_Data->category ?? CATEGORY_UNDEFINED) ],
+					[ 'name'=>'language', 'class'=>'form-control form-control-sm', 'prefix'=>($new ? ':' : ''), 'query'=>Form::query('language',Session::isAccessible()), 'title'=>'Language', 'type'=>'select', 'value'=>($_Data->language ?? LANGUAGE_UNDEFINED) ],
+					[ 'name'=>'accesslevel', 'class'=>'form-control form-control-sm', 'prefix'=>($new ? ':' : ''), 'query'=>Form::query('accesslevel',Session::isAccessible()), 'title'=>'Access Level', 'type'=>'select', 'value'=>($_Data->accesslevel ?? ACCESS_PUBLIC) ]
 				]
 			],
 			'image-pane'=>[
 				'columns'=>[ 'left'=>'col-12 col-md-6','right'=>'col-12 col-md-6' ],
 				'left'=>[
-					[ 'name'=>'description', 'class'=>'form-control', 'prefix'=>($new ? ':' : ''), 'required'=>false, 'size'=>4, 'title'=>ucwords($this->class).' Summary', 'type'=>'textarea' ]
+					[ 'name'=>'description', 'class'=>'form-control', 'prefix'=>($new ? ':' : ''), 'required'=>false, 'size'=>4, 'title'=>ucwords($this->class).' Summary', 'type'=>'textarea', 'value'=>($_Data->description ?? '') ]
 				],
 				'right'=>[
-					[ 'name'=>'tags', 'class'=>'form-control', 'prefix'=>($new ? ':' : ''), 'required'=>false, 'size'=>4, 'title'=>'Tags', 'type'=>'textarea' ]
+					[ 'name'=>'tags', 'class'=>'form-control', 'prefix'=>($new ? ':' : ''), 'required'=>false, 'size'=>4, 'title'=>'Tags', 'type'=>'textarea', 'value'=>($_Data->description ?? '') ]
 				]
 			],
 			'publishing-pane'=>[
 				'columns'=>[ 'left'=>'col-12 col-md-6','right'=>'col-12 col-md-6' ],
 				'left'=>[
-					[ 'name'=>'author', 'class'=>'form-control form-control-sm', 'default'=>Session::getUser(), 'query'=>Form::query('user',Session::isAccessible()), 'readonly'=>true, 'title'=>'Author', 'type'=>'select' ],
-					[ 'name'=>'editor', 'class'=>'form-control form-control-sm', 'default'=>USER_NOBODY, 'query'=>Form::query('user',Session::isAccessible()), 'readonly'=>true, 'title'=>'Editor', 'type'=>'select' ],
-					[ 'name'=>'publisher', 'class'=>'form-control form-control-sm', 'default'=>USER_NOBODY, 'query'=>Form::query('user',Session::isAccessible()), 'readonly'=>true, 'title'=>'Publisher', 'type'=>'select' ]
+					[ 'name'=>'author', 'class'=>'form-control form-control-sm', 'query'=>Form::query('user',Session::isAccessible()), 'readonly'=>(Application::getController()->cannotEdit($_Data->author ?? Session::getUser()) ?? true), 'title'=>'Author', 'type'=>'select', 'value'=>($_Data->author ?? Session::getUser()) ],
+					[ 'name'=>'editor', 'class'=>'form-control form-control-sm', 'query'=>Form::query('user',Session::isAccessible()), 'readonly'=>true, 'title'=>'Editor', 'type'=>'select', 'value'=>(Session::getUser() ?? USER_NOBODY) ],
+					[ 'name'=>'publisher', 'class'=>'form-control form-control-sm', 'query'=>Form::query('user',Session::isAccessible()), 'readonly'=>true, 'title'=>'Publisher', 'type'=>'select', 'value'=>($_Data->publisher ?? USER_NOBODY) ]
 				],
 				'right'=>[
-					[ 'name'=>'created', 'class'=>'form-control form-control-sm', 'default'=>'', 'readonly'=>true, 'title'=>'Created', 'type'=>'datetime' ],
-					[ 'name'=>'modified', 'class'=>'form-control form-control-sm', 'default'=>'', 'readonly'=>true, 'title'=>'Modified', 'type'=>'datetime' ],
-					[ 'name'=>'published', 'class'=>'form-control form-control-sm', 'default'=>'', 'readonly'=>true, 'title'=>'Published', 'type'=>'datetime' ]
+					[ 'name'=>'created', 'class'=>'form-control form-control-sm', 'readonly'=>true, 'title'=>'Created', 'type'=>'datetime', 'value'=>($_Data->created ?? '(now)') ],
+					[ 'name'=>'modified', 'class'=>'form-control form-control-sm', 'readonly'=>true, 'title'=>'Modified', 'type'=>'datetime', 'value'=> ($new ? '(never)' : '(now)') ],
+					[ 'name'=>'published', 'class'=>'form-control form-control-sm', 'readonly'=>true, 'title'=>'Published', 'type'=>'datetime', 'value'=> ($new ? '(never)' : '(now)') ]
 				]
 			],
 			'view-options-pane'=>[
 				'columns'=>[ 'left'=>'col-12 col-md-6','right'=>'col-12 col-md-6' ],
 				'left'=>[
-					[ 'name'=>'show-title', 'class'=>'form-control form-control-sm', 'default'=>SETTING_GLOBAL, 'list'=>[['#'=>SETTING_GLOBAL,'title'=>'Global setting'],['#'=>SETTING_HIDE,'title'=>'Hide'],['#'=>SETTING_SHOW,'title'=>'Show']], 'prefix'=>'@', 'title'=>'Show Title', 'type'=>'select' ],
-					[ 'name'=>'show-author', 'class'=>'form-control form-control-sm', 'default'=>SETTING_GLOBAL, 'list'=>[['#'=>SETTING_GLOBAL,'title'=>'Global setting'],['#'=>SETTING_HIDE,'title'=>'Hide'],['#'=>SETTING_AUTHOR,'title'=>'Show Author'],['#'=>SETTING_EDITOR,'title'=>'Show Editor'],['#'=>SETTING_PUBLISHER,'title'=>'Show Publisher']], 'prefix'=>'@', 'title'=>'Show Author', 'type'=>'select' ],
-					[ 'name'=>'show-category', 'class'=>'form-control form-control-sm', 'default'=>SETTING_GLOBAL, 'list'=>[['#'=>SETTING_GLOBAL,'title'=>'Global setting'],['#'=>SETTING_HIDE,'title'=>'Hide'],['#'=>SETTING_SHOW,'title'=>'Show']], 'prefix'=>'@', 'title'=>'Show Category', 'type'=>'select' ],
-					[ 'name'=>'show-tags', 'class'=>'form-control form-control-sm', 'default'=>SETTING_GLOBAL, 'list'=>[['#'=>SETTING_GLOBAL,'title'=>'Global setting'],['#'=>SETTING_HIDE,'title'=>'Hide'],['#'=>SETTING_SHOW,'title'=>'Show']], 'prefix'=>'@', 'title'=>'Show Tags', 'type'=>'select' ],
-					[ 'name'=>'show-date', 'class'=>'form-control form-control-sm', 'default'=>SETTING_GLOBAL, 'list'=>[['#'=>SETTING_GLOBAL,'title'=>'Global setting'],['#'=>SETTING_HIDE,'title'=>'Hide'],['#'=>SETTING_CREATEDDATE,'title'=>'Show Created Date'],['#'=>SETTING_MODIFIEDDATE,'title'=>'Show Modified Date'],['#'=>SETTING_PUBLISHEDDATE,'title'=>'Show Published Date']], 'prefix'=>'@', 'title'=>'Show Date', 'type'=>'select' ]
+					[ 'name'=>'show-title', 'class'=>'form-control form-control-sm', 'list'=>[['#'=>SETTING_GLOBAL,'title'=>'Global setting'],['#'=>SETTING_HIDE,'title'=>'Hide'],['#'=>SETTING_SHOW,'title'=>'Show']], 'prefix'=>'@', 'title'=>'Show Title', 'type'=>'select', 'value'=>($this->_Attribute['show-title'] ?? SETTING_GLOBAL) ],
+					[ 'name'=>'show-author', 'class'=>'form-control form-control-sm', 'list'=>[['#'=>SETTING_GLOBAL,'title'=>'Global setting'],['#'=>SETTING_HIDE,'title'=>'Hide'],['#'=>SETTING_AUTHOR,'title'=>'Show Author'],['#'=>SETTING_EDITOR,'title'=>'Show Editor'],['#'=>SETTING_PUBLISHER,'title'=>'Show Publisher']], 'prefix'=>'@', 'title'=>'Show Author', 'type'=>'select', 'value'=>($this->_Attribute['show-author'] ?? SETTING_GLOBAL) ],
+					[ 'name'=>'show-category', 'class'=>'form-control form-control-sm', 'list'=>[['#'=>SETTING_GLOBAL,'title'=>'Global setting'],['#'=>SETTING_HIDE,'title'=>'Hide'],['#'=>SETTING_SHOW,'title'=>'Show']], 'prefix'=>'@', 'title'=>'Show Category', 'type'=>'select', 'value'=>($this->_Attribute['show-category'] ?? SETTING_GLOBAL) ],
+					[ 'name'=>'show-tags', 'class'=>'form-control form-control-sm', 'list'=>[['#'=>SETTING_GLOBAL,'title'=>'Global setting'],['#'=>SETTING_HIDE,'title'=>'Hide'],['#'=>SETTING_SHOW,'title'=>'Show']], 'prefix'=>'@', 'title'=>'Show Tags', 'type'=>'select', 'value'=>($this->_Attribute['show-tags'] ?? SETTING_GLOBAL) ],
+					[ 'name'=>'show-date', 'class'=>'form-control form-control-sm', 'list'=>[['#'=>SETTING_GLOBAL,'title'=>'Global setting'],['#'=>SETTING_HIDE,'title'=>'Hide'],['#'=>SETTING_CREATEDDATE,'title'=>'Show Created Date'],['#'=>SETTING_MODIFIEDDATE,'title'=>'Show Modified Date'],['#'=>SETTING_PUBLISHEDDATE,'title'=>'Show Published Date']], 'prefix'=>'@', 'title'=>'Show Date', 'type'=>'select', 'value'=>($this->_Attribute['show-date'] ?? SETTING_GLOBAL) ]
 				],
 				'right'=>[
-					[ 'name'=>'show-image', 'class'=>'form-control form-control-sm', 'default'=>SETTING_GLOBAL, 'list'=>[['#'=>SETTING_GLOBAL,'title'=>'Global setting'],['#'=>SETTING_HIDE,'title'=>'Hide'],['#'=>SETTING_SHOW,'title'=>'Show']], 'prefix'=>'@', 'title'=>'Show Image', 'type'=>'select' ],
-					[ 'name'=>'position-image', 'class'=>'form-control form-control-sm', 'default'=>SETTING_GLOBAL, 'list'=>[['#'=>SETTING_GLOBAL,'title'=>'Global setting'],['#'=>SETTING_ABOVETITLE,'title'=>'Above Title'],['#'=>SETTING_BELOWTITLE,'title'=>'Below Title'],['#'=>SETTING_FLOATLEFT,'title'=>'Float Left'],['#'=>SETTING_FLOATRIGHT,'title'=>'Float Right']], 'prefix'=>'@', 'title'=>'Image Position', 'type'=>'select' ],
-					[ 'name'=>'show-info', 'class'=>'form-control form-control-sm', 'default'=>SETTING_GLOBAL, 'list'=>[['#'=>SETTING_GLOBAL,'title'=>'Global setting'],['#'=>SETTING_HIDE,'title'=>'Hide'],['#'=>SETTING_SHOW,'title'=>'Show']], 'prefix'=>'@', 'title'=>'Show Info', 'type'=>'select' ],
-					[ 'name'=>'position-info', 'class'=>'form-control form-control-sm', 'default'=>SETTING_GLOBAL, 'list'=>[['#'=>SETTING_GLOBAL,'title'=>'Global setting'],['#'=>SETTING_ABOVECONTENT,'title'=>'Above Content'],['#'=>SETTING_ABOVETITLE,'title'=>'Above Title'],['#'=>SETTING_BELOWTITLE,'title'=>'Below Title'],['#'=>SETTING_BELOWCONTENT,'title'=>'Below Content']], 'prefix'=>'@', 'title'=>'Info Position', 'type'=>'select' ],
-					[ 'name'=>'show-readmore', 'class'=>'form-control form-control-sm', 'default'=>SETTING_GLOBAL, 'list'=>[['#'=>SETTING_GLOBAL,'title'=>'Global setting'],['#'=>SETTING_HIDE,'title'=>'Hide'],['#'=>SETTING_SHOW,'title'=>'Show']], 'prefix'=>'@', 'title'=>'Show Read More', 'type'=>'select' ]
+					[ 'name'=>'show-image', 'class'=>'form-control form-control-sm', 'list'=>[['#'=>SETTING_GLOBAL,'title'=>'Global setting'],['#'=>SETTING_HIDE,'title'=>'Hide'],['#'=>SETTING_SHOW,'title'=>'Show']], 'prefix'=>'@', 'title'=>'Show Image', 'type'=>'select', 'value'=>($this->_Attribute['show-image'] ?? SETTING_GLOBAL) ],
+					[ 'name'=>'position-image', 'class'=>'form-control form-control-sm', 'list'=>[['#'=>SETTING_GLOBAL,'title'=>'Global setting'],['#'=>SETTING_ABOVETITLE,'title'=>'Above Title'],['#'=>SETTING_BELOWTITLE,'title'=>'Below Title'],['#'=>SETTING_FLOATLEFT,'title'=>'Float Left'],['#'=>SETTING_FLOATRIGHT,'title'=>'Float Right']], 'prefix'=>'@', 'title'=>'Image Position', 'type'=>'select', 'value'=>($this->_Attribute['position-image'] ?? SETTING_GLOBAL) ],
+					[ 'name'=>'show-info', 'class'=>'form-control form-control-sm', 'list'=>[['#'=>SETTING_GLOBAL,'title'=>'Global setting'],['#'=>SETTING_HIDE,'title'=>'Hide'],['#'=>SETTING_SHOW,'title'=>'Show']], 'prefix'=>'@', 'title'=>'Show Info', 'type'=>'select', 'value'=>($this->_Attribute['show-info'] ?? SETTING_GLOBAL) ],
+					[ 'name'=>'position-info', 'class'=>'form-control form-control-sm', 'list'=>[['#'=>SETTING_GLOBAL,'title'=>'Global setting'],['#'=>SETTING_ABOVECONTENT,'title'=>'Above Content'],['#'=>SETTING_ABOVETITLE,'title'=>'Above Title'],['#'=>SETTING_BELOWTITLE,'title'=>'Below Title'],['#'=>SETTING_BELOWCONTENT,'title'=>'Below Content']], 'prefix'=>'@', 'title'=>'Info Position', 'type'=>'select', 'value'=>($this->_Attribute['position-info'] ?? SETTING_GLOBAL) ],
+					[ 'name'=>'show-readmore', 'class'=>'form-control form-control-sm', 'list'=>[['#'=>SETTING_GLOBAL,'title'=>'Global setting'],['#'=>SETTING_HIDE,'title'=>'Hide'],['#'=>SETTING_SHOW,'title'=>'Show']], 'prefix'=>'@', 'title'=>'Show Read More', 'type'=>'select', 'value'=>($this->_Attribute['show-readmore'] ?? SETTING_GLOBAL) ]
 				]
 			]
 		];
